628bb05a2e697564a92d4978087dfd7590f42db6
unknown <hamdyramzy34@gmail.com>
2021-12-09 05:37:40 +0200

    Edited Style

--- static/js/dark.js
+++ static/js/dark.js
@@ -1,42 +1,47 @@
+const darkBtn = document.querySelector('.fass');
+const icon = document.getElementById('icon')
+setIcon = localStorage.getItem('sun');
 
-let darkMode = localStorage.getItem('darkMode');
-let darkModeButton = localStorage.getItem('darkModeButton');
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const darkMode = () => {
+    document.body.classList.toggle('dark')
+}
 
-darkModeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode');
-    if (darkMode !== 'enabled') {   
-        enableDarkMode();
-    }
-    else {       
-        disableDarkMode();
+darkBtn.addEventListener('click', () => {
+    // Get the value of the "dark" item from the local storage on every click
+    setDarkMode = localStorage.getItem('dark');
+
+    if(setDarkMode !== "on") {
+        darkMode();
+        icon.classList.remove('bi-moon');
+        icon.classList.add('bi-brightness-low-fill');
+        // Set the value of the itwm to "on" when dark mode is on
+        setDarkMode = localStorage.setItem('dark', 'on');
+        setIcon = localStorage.setItem('sun', 'on');
+    } else {
+        darkMode();
+        // Set the value of the item to  "null" when dark mode if off
+        icon.classList.remove('bi-brightness-low-fill');
+        icon.classList.add('bi-moon');
+        setDarkMode = localStorage.setItem('dark', null);
+        setIcon = localStorage.setItem('sun', null);
     }
 });
 
+// Get the value of the "dark" item from the local storage
+let setDarkMode = localStorage.getItem('dark');
 
-const enableDarkMode = () => {
-    document.body.classList.add('darkmode');
-    darkModeToggle.classList.add('dark-mode-toggle');
-    localStorage.setItem('darkMode', 'enabled');
-    localStorage.setItem('darkModeButton', 'disabled');
+// Check dark mode is on or off on page reload
+if(setDarkMode === 'on') {
+    darkMode();
+    icon.classList.remove('bi-moon');
+    icon.classList.add('bi-brightness-low-fill');
 }
 
-const disableDarkMode = () => {
-    document.body.classList.remove('darkmode');
-    darkModeToggle.classList.remove('dark-mode-toggle');
-    localStorage.setItem('darkMode', null);
-    localStorage.setItem('darkModeButton', 'enabled');
-}
-
-if (darkModeButton === 'enabled') {
-    darkModeToggle.classList.remove('dark-mode-toggle');   
+if (setIcon === 'on') {
+    icon.classList.remove('bi-moon');
+    icon.classList.add('bi-brightness-low-fill');  
 }
 else{
-    darkModeToggle.classList.add('dark-mode-toggle');
+    icon.classList.remove('bi-brightness-low-fill');
+    icon.classList.add('bi-moon');
 }
-
-if (darkMode === 'enabled') {
-    document.body.classList.add('darkmode');
-    enableDarkMode();
-}
-
