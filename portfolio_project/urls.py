1a226407086af5e2202f4d5ea89988dcdae0f57f
unknown <hamdyramzy34@gmail.com>
2022-02-28 08:52:06 +0200

    Static root changes

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -26,9 +26,7 @@ urlpatterns = [
     path('', include('authentication.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
+urlpatterns += static(settings.STATIC_URL,                          
                         document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                         document_root=settings.MEDIA_ROOT)
