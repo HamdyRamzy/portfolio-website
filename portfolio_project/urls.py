3ff11c51ca52a8262ef2d7b0eddf28139dd195d8
unknown <hamdyramzy34@gmail.com>
2022-03-01 01:17:04 +0200

    change media root

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -26,5 +26,8 @@ urlpatterns = [
     path('', include('authentication.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
