ebcd07107d32f8d890b7b57a99df3830370fd921
unknown <hamdyramzy34@gmail.com>
2022-02-28 08:56:08 +0200

    Static root changes

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -222,7 +222,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
