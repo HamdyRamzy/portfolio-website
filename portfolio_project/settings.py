1f0cce23fe52e6cb50f485257b3a1ea622e1d643
unknown <hamdyramzy34@gmail.com>
2022-02-28 23:42:28 +0200

    change depug

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -226,7 +226,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
