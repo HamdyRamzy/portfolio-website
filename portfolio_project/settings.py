bf9da9d9357c227d2eb425155a3cd127e5f6e4a6
unknown <hamdyramzy34@gmail.com>
2022-03-01 02:54:05 +0200

    added social contacts

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -222,12 +222,12 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # Add these new lines
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT =  BASE_DIR / 'static'
-MEDIA_ROOT =  BASE_DIR / 'media'
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT =  BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
